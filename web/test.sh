7dd8df9d203e49120e792b8ea88ba3c553b33957
Jason Chin <cschin@infoecho.net>
2023-12-29 12:30:23 -0800

    update the URL for quick test

--- web/test.sh
+++ web/test.sh
@@ -1,1 +1,1 @@
-curl  http://127.0.0.1:3000/api/post_query -X POST -H "Content-Type: application/json"  -d '{"text":"NARS2", "topn":10}'
+curl  http://127.0.0.1:3000/api/post_query_for_similarity_search -X POST -H "Content-Type: application/json"  -d '{"text":"NARS2", "topn":10}'
